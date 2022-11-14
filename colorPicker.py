d6fbd0d4c025625aa0830beca0a5cf3397758533
EvanRothenbacher10 <825585@mystma.org>
2022-11-14 09:49:44 -0600

    units and about

--- colorPicker.py
+++ colorPicker.py
@@ -3,21 +3,24 @@ from tkinter import *
 scrn = Tk()
 scrn.geometry("600x400")
 
-def colorCode():
-    if len(colorEntry.get()) == 6:
-        scrn.config(bg=("#" + colorEntry.get()))
-    else:
-        raise ValueError("Length must be 6 characters")
-
 def tryChange():
     try:
         colorCode()
     except:
-        errorLabel = Label(text="Length must be 6 characters")
+        errorLabel.config(text="Length must be 6 characters")
         errorLabel.grid(row=2, column=2)
+
+def colorCode():
+    if len(colorEntry.get()) == 6:
+        scrn.config(bg=("#" + colorEntry.get()))
+        errorLabel.grid_forget()
+    else:
+        raise ValueError("Length must be 6 characters")
 
 colorLabel = Label(text="Color Code:")
 colorLabel.grid()
+
+errorLabel = Label()
 
 colorEntry = Entry()
 colorEntry.grid(row=0, column=1)
