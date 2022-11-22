814e25111fed33c209e9f17f973f7846994b03b9
EvanRothenbacher10 <825585@mystma.org>
2022-11-22 09:15:08 -0600

    4

--- unitsCalculator.py
+++ unitsCalculator.py
@@ -3,9 +3,17 @@ from tkinter import *
 scrn = Tk()
 
 def fcFunc():
-    print(fcEntry.get() * (9/5) + 32)
-    outputLabel.config(text=str(fcEntry.get() * (9/5) + 32))
-    
+    outputLabel.config(text=int(fcEntry.get()) * (9/5) + 32)
+
+def miFunc():
+    outputLabel.config(text=int(miEntry.get()) * 100)
+
+def pkgFunc():
+    outputLabel.config(text=int(pkgEntry.get()) * 0.4536)
+
+def deFunc():
+    outputLabel.config(text=int(deEntry.get()) * 0.97)
+
 fcLabel = Label(text="Fahrenheit to Celcius | ")
 fcLabel.grid(column=0, row=0)
 fcEntry = Entry()
@@ -17,23 +25,24 @@ miLabel = Label(text="Millimeters to Centimeters | ")
 miLabel.grid(column=1, row=0)
 miEntry = Entry()
 miEntry.grid(column=1, row=1)
-miCalc = Button(text="Calculate")
+miCalc = Button(text="Calculate", command=miFunc)
 miCalc.grid(column=1, row=2)
 
 pkgLabel = Label(text="Pounds to Kilograms | ")
 pkgLabel.grid(column=2, row=0)
 pkgEntry = Entry()
 pkgEntry.grid(column=2, row=1)
-pkgCalc = Button(text="Calculate")
+pkgCalc = Button(text="Calculate", command=pkgFunc)
 pkgCalc.grid(column=2, row=2)
 
 deLabel = Label(text="Dollars to Euros ")
 deLabel.grid(column=3, row=0)
 deEntry = Entry()
 deEntry.grid(column=3, row=1)
-deCalc = Button(text="Calculate")
+deCalc = Button(text="Calculate", command=deFunc)
 deCalc.grid(column=3, row=2)
 
 outputLabel = Label(text="")
 outputLabel.grid(row=3)
+
 scrn.mainloop()
